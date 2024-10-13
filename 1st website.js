f657604689ab207821d3d306e67d35e92c998e22
SURTUR-24 <130675758+SURTUR-24@users.noreply.github.com>
2024-10-13 16:25:49 +0530

    1st website.js

--- 1st website.js
+++ 1st website.js
@@ -3,6 +3,10 @@ const themeButton = document.getElementById('theme');
 if (themeButton) {
     themeButton.addEventListener('click', () => {
         document.body.classList.toggle('dark');
+        themeButton.classList.add('clicked');
+        setTimeout(() => {
+            themeButton.classList.remove('clicked');
+        }, 300); 
     });
 } else {
     console.error('Theme button not found.');
